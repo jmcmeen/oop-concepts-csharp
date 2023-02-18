5dc2ea5b5d2fedf448b402536e58b85d368bee85
John McMeen <jnmcmeen@northeaststate.edu>
2023-02-18 01:05:29 -0500

    Basic DateTime example

---
+++ XX-DateTime/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace XX_DateTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<DateTime> list = new List<DateTime>();
+
+            DateTime date1 = DateTime.Now;
+            DateTime date2 = DateTime.Now;
+            DateTime date3 = DateTime.UtcNow;
+            DateTime date4 = DateTime.Today;
+
+            list.Add(date1);
+            list.Add(date2);
+            list.Add(date3);
+            list.Add(date4);
+
+            foreach(DateTime dt in list)
+            {
+                Console.WriteLine(dt);
+            }
+        }
+    }
+}
