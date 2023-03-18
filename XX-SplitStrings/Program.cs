f54d652936761dbcb3a22b1845f5603842c8d02e
John McMeen <jnmcmeen@northeaststate.edu>
2023-03-18 11:50:23 -0400

    Small splitting strings demonstration

---
+++ XX-SplitStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace XX_SplitStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string aSentence = "Hello World! How are you today!";
+
+            string[] aSplitSentence = aSentence.Split(" ");
+
+            foreach (string s in aSplitSentence)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
