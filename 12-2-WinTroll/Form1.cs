c1579fcd347541cd91100a152f9a72c1ff7c9fdd
John McMeen <jnmcmeen@northeaststate.edu>
2023-04-05 00:21:41 -0400

    WinTroll v1.0

---
+++ 12-2-WinTroll/Form1.cs
@@ -0,0 +1,20 @@
+namespace XX_BasicWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.button1.MouseEnter += OnMouseEnterButton1;
+        }
+
+        private void OnMouseEnterButton1(object sender, EventArgs e)
+        {
+            int x = new Random().Next(0 + button1.Width, this.Width - button1.Width*2);
+            int y = new Random().Next(0 + button1.Height, this.Height - button1.Height * 2);
+
+            button1.Location = new Point(x, y);
+            
+        }
+    }
+}
