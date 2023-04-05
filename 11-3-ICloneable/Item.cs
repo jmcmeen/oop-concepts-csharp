a49e8ab6496e82087a9d14182f66c404d47bb111
John McMeen <jnmcmeen@northeaststate.edu>
2023-04-04 22:06:00 -0400

    ICloneable examples

---
+++ 11-3-ICloneable/Item.cs
@@ -0,0 +1,44 @@
+﻿namespace _11_1_CopyConstructors
+{
+    internal class Item : ICloneable
+    {
+        public string? Description { get; set; }
+
+        public float Cost { get; set; }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Item(){}
+
+        /// <summary>
+        /// Parametrized constructor
+        /// </summary>
+        /// <param name="description"></param>
+        /// <param name="cost"></param>
+        public Item(string? description, float cost)
+        {
+            Description = description;
+            Cost = cost;
+        }
+
+        /// <summary>
+        /// ToString
+        /// </summary>
+        /// <returns></returns>
+        public override string? ToString()
+        {
+            return $"{Description} {Cost}";
+        }
+
+        /// <summary>
+        /// Clone
+        /// </summary>
+        /// <returns></returns>
+        public object Clone()
+        {
+            //create and return a new Item reference, passing in this Item's properties
+            return new Item(Description, Cost);
+        }
+    }
+}
---
+++ 11-3-ICloneable/Program.cs
@@ -0,0 +1,20 @@
+﻿using _11_1_CopyConstructors;
+
+namespace _11_3_ICloneable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //First there is one Item
+            Item itemOne = new Item("Sword", 235.2f);
+
+            Console.WriteLine(itemOne);
+
+            //Now we can create a copy using Item's Clone Method, since it implements the ICloneable interface
+            Item itemTwo = (Item)itemOne.Clone();
+
+            Console.WriteLine(itemTwo);
+        }
+    }
+}
