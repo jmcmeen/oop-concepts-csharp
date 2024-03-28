4b92926864f380cfec4050eec262a2bd7af3dbaa
John McMeen <johnmcmeen@gmail.com>
2024-03-27 21:33:24 -0400

    the simplest async await example, will refactor

---
+++ 10-1-AsyncAwait/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _10_1_AsyncAwait
+{
+    static class Program
+    {
+        static void Main()
+        {
+            _ = Method1();
+            Method2();
+            Console.ReadKey();
+        }
+
+        public static async Task Method1()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine(" Method 1");
+                    // Do something
+                    Task.Delay(100).Wait();
+                }
+            });
+        }
+
+
+        public static void Method2()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine(" Method 2");
+                // Do something
+                Task.Delay(1000).Wait();
+            }
+        }
+    }
+}
