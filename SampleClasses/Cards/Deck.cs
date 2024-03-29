e1cec59281da5d58e456f351a94229b10530378c
John McMeen <johnmcmeen@gmail.com>
2024-03-28 23:52:56 -0400

    Update Deck.cs

--- SampleClasses/Cards/Deck.cs
+++ SampleClasses/Cards/Deck.cs
@@ -1,6 +1,4 @@
-﻿using DeckOfCards.Utilities;
-
-namespace SampleClasses.Cards
+﻿namespace SampleClasses.Cards
 {
     /// <summary>
     /// Models a deck of playing cards
@@ -8,62 +6,25 @@ namespace SampleClasses.Cards
     internal class Deck
     {
         //public fields
-        private Stack<Card> _cards;
+        private List<Card> _cards;
 
         /// <summary>
         /// Default constructor
         /// </summary>
-        public Deck(bool shuffle = false)
+        public Deck()
         {
             //initialize memory for the card list
-            _cards = new Stack<Card>();
+            _cards = new List<Card>();
 
             //Create all 52 combinations of Suit and Rank
             foreach (Suit s in Enum.GetValues(typeof(Suit)))
             {
                 foreach (Rank r in Enum.GetValues(typeof(Rank)))
                 {
-                    _cards.Push(new Card(s, r));
+                    _cards.Add(new Card(s, r));
                 }
             }
-
-            if (shuffle) { _cards.Shuffle(); }
         }
 
-        /// <summary>
-        /// Creates a list of cards taken from the deck
-        /// </summary>
-        /// <param name="numCards">The number of cards to take from the top of the card list</param>
-        /// <returns>A new list of cards of size numCards</returns>
-        public Hand? DealHand(uint numCards)
-        {
-            Hand? h = null;
-
-            if (_cards.Count >= numCards)
-            {
-                h = new Hand();
-
-                for (int i = 0; i < numCards; i++)
-                {
-                    h.AddCard(_cards.Pop());
-                }
-            }
-            return h;
-        }
-
-        public Card? DealCard()
-        {
-            Card? c;
-            _cards.TryPop(out c);
-            return c;
-        }
-
-        /// <summary>
-        /// Shuffles the deck
-        /// </summary>
-        public void Shuffle()
-        {
-            _cards.Shuffle();
-        }
     }
 }
