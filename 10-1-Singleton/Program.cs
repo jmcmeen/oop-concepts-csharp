b3fa6caaeee12d234d59e099b36079718963e31d
John McMeen <johnmcmeen@gmail.com>
2024-04-05 16:48:11 -0400

    added Singleton example

---
+++ 10-1-Singleton/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _10_1_Singleton
+{
+    /// <summary>
+    /// Demonstrates the Singleton design pattern
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// Program entry point
+        /// </summary>
+        static void Main()
+        {
+            /* The Singleton Design Pattern is a creational design pattern that ensures 
+             * a class has only one instance, providing a global point of access to it. 
+             * It can be useful in situations where you want to limit the number of 
+             * instances of a class, or when you need a single object to coordinate 
+             * actions across the system. */
+
+            // Some other useful forms of the Singleton design pattern can be found here:
+            // https://www.c-sharpcorner.com/UploadFile/8911c4/singleton-design-pattern-in-C-Sharp/
+
+            Singleton singleton = Singleton.Instance;
+            Singleton singleton2 = Singleton.Instance;
+
+            if (singleton == singleton2)
+            {
+                Console.WriteLine("These references are identical.");
+            }
+        }
+    }
+}
---
+++ 10-1-Singleton/Singleton.cs
@@ -0,0 +1,33 @@
+﻿namespace _10_1_Singleton
+{
+    /// <summary>
+    /// Singleton design pattern
+    /// </summary>
+    public sealed class Singleton
+    {
+        /// <summary>
+        /// The singleton static instance
+        /// </summary>
+        private static Singleton? instance = null;
+
+        /// <summary>
+        /// Private default constructor can only be call by Singleton
+        /// </summary>
+        private Singleton() { }
+   
+        /// <summary>
+        /// Returns the previous instance or if no previous instance a new one.
+        /// </summary>
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+    }
+}
