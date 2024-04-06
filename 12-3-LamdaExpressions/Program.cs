ab337d4f609c2210de419872e7753ed463fb853c
John McMeen <johnmcmeen@gmail.com>
2024-04-06 01:53:49 -0400

    lambda expressions

---
+++ 12-3-LamdaExpressions/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _12_3_LamdaExpressions
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Add(2, 2));
+            Console.WriteLine(Multiply(3,3));
+        }
+
+        static int Add (int x, int y) => x + y;
+
+        static int Multiply (int x, int y) => x * y;
+    }
+}
