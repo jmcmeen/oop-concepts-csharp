81c5154071515cb852556ac9cbb65dcd10500ba3
John McMeen <johnmcmeen@gmail.com>
2024-10-07 11:52:53 -0400

    Update Pen.cs

--- 05-3-AbstractClasses/Pen.cs
+++ 05-3-AbstractClasses/Pen.cs
@@ -7,5 +7,9 @@ namespace _05_3_AbstractClasses
 {
     public class Pen : WritingTool
     {
+        public override void Write()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
